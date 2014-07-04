8b48d23e6a858d6610e0759750fadea5a3ff8e7c
Kyle Traff <ktraff@Kyles-MacBook-Pro.local>
2014-07-04 14:32:06 -0600

    rotating coins for points

--- assets/game.js
+++ assets/game.js
@@ -1,6 +1,10 @@
 // Create the canvas
-var canvas = document.createElement("canvas");
-var ctx = canvas.getContext("2d");
+var canvas = document.createElement('canvas');
+var ctx = canvas.getContext('2d');
+// Used for coin animation.
+var frameIndex = 0;
+var tickCount = 0;
+
 canvas.width = 512;
 canvas.height = 480;
 document.body.appendChild(canvas);
@@ -9,115 +13,216 @@ document.body.appendChild(canvas);
 var bgReady = false;
 var bgImage = new Image();
 bgImage.onload = function () {
-	bgReady = true;
+    bgReady = true;
 };
-bgImage.src = "images/background.png";
+bgImage.src = 'assets/images/background.png';
 
-// Hero image
-var heroReady = false;
-var heroImage = new Image();
-heroImage.onload = function () {
-	heroReady = true;
+// Car image
+var carReady = false;
+var carImage = new Image();
+carImage.onload = function () {
+    carReady = true;
 };
-heroImage.src = "images/hero.png";
+carImage.src = 'assets/images/player-car.svg';
 
-// Monster image
-var monsterReady = false;
-var monsterImage = new Image();
-monsterImage.onload = function () {
-	monsterReady = true;
+// Coins
+var coinsReady = 0;
+var numCoins = 5;
+var coinImageSources = ['coin_copper.png', 'coin_silver.png', 'coin_gold.png'];
+var coins = [];
+// Determines when all coin sprites are loaded.
+var coinLoaded = function () {
+    if (!isNaN(coinsReady) && coinsReady >= coinImageSources.length - 1) {
+        // All coins have been loaded.
+        coinsReady = true;
+    }
+    else { // Increment number of total coins loaded.
+        coinsReady++;
+    }
 };
-monsterImage.src = "images/monster.png";
+// Returns a random x coodinate on the road.
+var getRoadX = function () {
+    return 32 + Math.max(Math.random(), 120);
+};
+var getRoadY = function () {
+    return 32 + (Math.random() * (canvas.height - 64));
+};
+// Renders a rotating coin.
+function sprite (options) {
+    
+    var that = {},
+        frameIndex = 0,
+        tickCount = 0,
+        ticksPerFrame = options.ticksPerFrame || 0,
+        numberOfFrames = options.numberOfFrames || 1;
+    
+    that.context = options.context;
+    that.x = options.x || 0;
+    that.y = options.y || 0;
+    that.width = options.width;
+    that.height = options.height;
+    that.image = options.image;
+    
+    that.update = function () {
+
+        tickCount += 1;
+
+        if (tickCount > ticksPerFrame) {
+
+            tickCount = 0;
+            
+            // If the current frame index is in range
+            if (frameIndex < numberOfFrames - 1) {
+                // Go to the next frame
+                frameIndex += 1;
+            } else {
+                frameIndex = 0;
+            }
+        }
+    };
+    
+    that.render = function () {
+    
+      // Clear the canvas
+      //that.context.clearRect(0, 0, that.width, that.height);
+      
+      // Draw the animation
+      that.context.drawImage(
+        that.image,
+        frameIndex * that.width / numberOfFrames,
+        0,
+        that.width / numberOfFrames,
+        that.height,
+        that.x,
+        that.y,
+        that.width / numberOfFrames,
+        that.height);
+    };
+    
+    return that;
+}
+for (var i = 0; i < numCoins; i++) {
+    var coinImage = new Image();
+    coinImage.onload = coinLoaded;
+    var srcIdx = parseInt((Math.random() * 3), 10);
+    coinImage.src = 'assets/images/' + coinImageSources[srcIdx];
+    var coin = sprite({
+        context: ctx,
+        x: 32 + 50 + ((Math.random() * 150) + 1),
+        y: 32 + (Math.random() * (canvas.height - 64)),
+        width: 256,
+        height: 32,
+        image: coinImage,
+        numberOfFrames: 8,
+        ticksPerFrame: 6
+    });
+    coins.push(coin);
+}
 
 // Game objects
-var hero = {
-	speed: 256 // movement in pixels per second
+var car = {
+    speed: 256, // movement in pixels per second
+    x: canvas.width / 2,
+    y: canvas.height / 2
 };
-var monster = {};
-var monstersCaught = 0;
+var coinsCaught = 0;
 
 // Handle keyboard controls
 var keysDown = {};
 
-addEventListener("keydown", function (e) {
-	keysDown[e.keyCode] = true;
+addEventListener('keydown', function (e) {
+    keysDown[e.keyCode] = true;
 }, false);
 
-addEventListener("keyup", function (e) {
-	delete keysDown[e.keyCode];
+addEventListener('keyup', function (e) {
+    delete keysDown[e.keyCode];
 }, false);
 
-// Reset the game when the player catches a monster
+// Reset the game when the player catches a coin
 var reset = function () {
-	hero.x = canvas.width / 2;
-	hero.y = canvas.height / 2;
-
-	// Throw the monster somewhere on the screen randomly
-	monster.x = 32 + (Math.random() * (canvas.width - 64));
-	monster.y = 32 + (Math.random() * (canvas.height - 64));
 };
 
 // Update game objects
 var update = function (modifier) {
-	if (38 in keysDown) { // Player holding up
-		hero.y -= hero.speed * modifier;
-	}
-	if (40 in keysDown) { // Player holding down
-		hero.y += hero.speed * modifier;
-	}
-	if (37 in keysDown) { // Player holding left
-		hero.x -= hero.speed * modifier;
-	}
-	if (39 in keysDown) { // Player holding right
-		hero.x += hero.speed * modifier;
-	}
+    var key;
 
-	// Are they touching?
-	if (
-		hero.x <= (monster.x + 32)
-		&& monster.x <= (hero.x + 32)
-		&& hero.y <= (monster.y + 32)
-		&& monster.y <= (hero.y + 32)
-	) {
-		++monstersCaught;
-		reset();
-	}
+    if (38 in keysDown) { // Player holding up
+        car.y -= car.speed * modifier;
+    }
+    if (40 in keysDown) { // Player holding down
+        car.y += car.speed * modifier;
+    }
+    if (37 in keysDown) { // Player holding left
+        car.x -= car.speed * modifier;
+    }
+    if (39 in keysDown) { // Player holding right
+        car.x += car.speed * modifier;
+    }
+
+    for (key in coins) {
+        coins[key].update();
+    }
+
+    for (key in coins) {
+        var coin = coins[key];
+        // Are they touching?
+        if (
+            car.x <= (coin.x + 24) &&
+            coin.x <= (car.x + 24) &&
+            car.y <= (coin.y + 24) &&
+            coin.y <= (car.y + 24)
+        ) {
+            var value = coin.image.src.indexOf('gold')   !== -1 ? 3 :
+                        coin.image.src.indexOf('silver') !== -1 ? 2 : 1;
+            coinsCaught += value;
+
+            // Change the coin's position and value.
+            var srcIdx = parseInt((Math.random() * 3), 10);
+            coin.image.src = 'assets/images/' + coinImageSources[srcIdx];
+            coin.x = 32 + 50 + ((Math.random() * 300) + 1);
+            coin.y = 32 + (Math.random() * (canvas.height - 64));
+
+            reset();
+        }
+    }
 };
 
 // Draw everything
 var render = function () {
-	if (bgReady) {
-		ctx.drawImage(bgImage, 0, 0);
-	}
+    if (bgReady) {
+        ctx.drawImage(bgImage, 0, 0);
+    }
 
-	if (heroReady) {
-		ctx.drawImage(heroImage, hero.x, hero.y);
-	}
+    if (carReady) {
+        ctx.drawImage(carImage, car.x, car.y, 32, 32);
+    }
 
-	if (monsterReady) {
-		ctx.drawImage(monsterImage, monster.x, monster.y);
-	}
+    if (coinsReady) {
+        for (var key in coins) {
+            coins[key].render();
+        }
+    }
 
-	// Score
-	ctx.fillStyle = "rgb(250, 250, 250)";
-	ctx.font = "24px Helvetica";
-	ctx.textAlign = "left";
-	ctx.textBaseline = "top";
-	ctx.fillText("Goblins caught: " + monstersCaught, 32, 32);
+    // Score
+    ctx.fillStyle = 'rgb(250, 250, 250)';
+    ctx.font = '24px Helvetica';
+    ctx.textAlign = 'left';
+    ctx.textBaseline = 'top';
+    ctx.fillText('Coins: ' + coinsCaught, 32, 32);
 };
 
 // The main game loop
 var main = function () {
-	var now = Date.now();
-	var delta = now - then;
+    var now = Date.now();
+    var delta = now - then;
 
-	update(delta / 1000);
-	render();
+    update(delta / 1000);
+    render();
 
-	then = now;
+    then = now;
 
-	// Request to do this again ASAP
-	requestAnimationFrame(main);
+    // Request to do this again ASAP
+    requestAnimationFrame(main);
 };
 
 // Cross-browser support for requestAnimationFrame
